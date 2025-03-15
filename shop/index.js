da4bf8c6c7a228d2de87d007800540c287be87ac
brightonchebor <cheborbrighton805@gmail.com>
2025-03-15 14:31:29 +0300

    cn

---
+++ shop/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const app_port = process.env.APP_PORT;
+const app_name = process.env.APP_NAME;
+const origins = JSON.parse(process.env.ORIGINS);
+
+module.exports = {
+  app_port,
+  app_name,
+  origins,
+};
