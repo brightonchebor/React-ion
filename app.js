87a19fe686395a8132064720d6ee92e7584a2753
brightonchebor <cheborbrighton805@gmail.com>
2025-03-15 12:52:53 +0300

    cn

--- app.js
+++ app.js
@@ -14,5 +14,16 @@ function test(){
     console.log('Local Scope', a, b, c)
 }
 
+let global = () => {
+
+    // Another Global Scope
+    var a = 7
+    var b = 8
+    var c = 9
+
+    console.log('Another Global Scope', a, b, c)
+}
+
 test()
 console.log('Global Scope', a, b, c)
+global()
