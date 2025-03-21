fb80ca69289544fadcdda062283c51f26290a089
brightonchebor <cheborbrighton805@gmail.com>
2025-03-21 21:02:48 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useSyncExternalStore } from "react"
 
 const Card = ({title}) => {
+
+  const [hasLiked, setHasLiked] = useState(initialState=false)
+
   return( 
     <div className='card'>
       <h2> {title} </h2>
+      <button onClick={() => setHasLiked(true)}>
+        Like
+      </button>
+
     </div>
   )
 }
 
 const App = () => {
+
+  
   return(
     <div className='card-container'>
-      < Card title='Love, Simon' />
       < Card title='After We Collided' />
+      < Card title='Love, Simon' />
       < Card title='Five Feet Apart' />
     </div>
 
