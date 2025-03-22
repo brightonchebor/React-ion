a4ffd1a97addab73035f4f1e44a0da913a46854d
brightonchebor <cheborbrighton805@gmail.com>
2025-03-22 20:55:40 +0300

    Cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -8,8 +8,8 @@ const Card = ({title}) => {
     <div className='card'>
 
       <h2> {title} </h2>
-      <button onClick={() => setHasLiked(true)}>
-        Like
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍' }
       </button>
 
     </div>
