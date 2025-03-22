599343023a3799b4f9e2a5127f19885f9295809a
brightonchebor <cheborbrighton805@gmail.com>
2025-03-22 23:05:36 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -5,13 +5,15 @@ const Card = ({title}) => {
   // useStateb sytanx (it's a hook)
   const [hasLiked, setHasLiked] = useState(false) 
 
+  const [count, setCount] = useState(0)
+
   useEffect(() => {
     console.log(`${title} has been liked: ${hasLiked}`)
   })
   return( 
-    <div className='card'>
+    <div className='card' onClick={() => setCount(count + 1)}>
 
-      <h2> {title} </h2>
+      <h2> {title} <br /> {count} </h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '🤍' }
       </button>
