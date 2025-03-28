f0b834e9309fe490623259233d96873670ec60de
brightonchebor <cheborbrighton805@gmail.com>
2025-03-28 07:39:45 +0300

    cn

--- mosh.js
+++ mosh.js
@@ -1,4 +1,10 @@
 let selectedColors = ['red', 'blue'];
 
-console.log(selectedColors);
-console.log(selectedColors[0])
+// console.log(selectedColors);
+// console.log(selectedColors[0])
+
+const greet = (name, firstName) => {
+    console.log('Hello ' + name + ' ' + firstName);
+}
+
+console.log(greet('Brighton', 'Chebor'));
