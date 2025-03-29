193a799ef8bf5645d0cb70e8ac5ae5078f9ad79d
brightonchebor <cheborbrighton805@gmail.com>
2025-03-29 09:13:16 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,45 +1,5 @@
-import React, { useState, useEffect } from "react"
-
-const Card = ({title}) => {
-
-  // useStateb sytanx (it's a hook)
-  const [hasLiked, setHasLiked] = useState(false) 
-
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`)
-  }, [hasLiked]);
-  
-  return( 
-    <div className='card' onClick={() => setCount(count + 1)}>
-
-      <h2> {title} <br /> {count} </h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍' }
-      </button>
-
-    </div>
-  )
-}
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const increament = () => setCount(count + 1);
-  const decreament = () => setCount(count - 1);
-
-  return(
-    <div className="card">
-      <h2>My count:{count}</h2>
-      <button onClick={increament} style={{color:'white'}}>+</button>
-      <button onClick={decreament} style={{color:'white'}}>-</button>
-    </div>
-  )
-
-
-}
-
+import Card from "./components/Card"
+import Counter from "./components/Counter"
 
 
 const App = () => {
