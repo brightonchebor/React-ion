7fa7c812416cc034b7ab3da12f6d4b2228c36a10
brightonchebor <cheborbrighton805@gmail.com>
2025-03-29 11:14:14 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,22 +1,31 @@
 import Card from "./components/Card"
 import Counter from "./components/Counter"
+import Navbar from "./components/Navbar"
+import "./App.css";
 
 
 const App = () => {
 
   
   return(
-    <div className='card-container'>
+    // <div className='card-container'>
 
-      < Card title='After We Collided' />
-      < Card title='Love, Simon' />
-      < Card title='Five Feet Apart' />
-      < Card title='Squid Game' />
+    //   < Card title='After We Collided' />
+    //   < Card title='Love, Simon' />
+    //   < Card title='Five Feet Apart' />
+    //   < Card title='Squid Game' />
 
-      <Counter />
+    //   <Counter />
       
-    </div>
+    // </div>
 
+    <>
+      <Navbar />
+      <div className="h-screen relative bg-[url('/src/assets/mountain.jpg')] bg-cover bg-center before:absolute before:inset-0 before:bg-slate-900 before:opacity-50">
+        <div className="z-50"></div>
+      
+      </div>
+    </>
   )
 }
 export default App
