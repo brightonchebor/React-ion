88bb5d0073ad2e46a6ea8a904c9fdaa6b54e9e02
brightonchebor <cheborbrighton805@gmail.com>
2025-03-31 15:01:19 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
     <>
       <Navbar />
-      <div className="h-screen relative bg-[url('/src/assets/mountain.jpg')] bg-cover bg-center before:absolute before:inset-0 before:bg-slate-900 before:opacity-50">
+      <div className="h-screen  bg-[url('/src/assets/mountain.jpg')] bg-cover bg-center before:absolute before:inset-0 before:bg-slate-900 before:opacity-50">
         <div className="z-50"></div>
       
       </div>
