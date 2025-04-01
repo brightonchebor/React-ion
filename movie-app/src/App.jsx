96c93a6635f2c9fa8ee6c8afc72ba94e6b4ebd24
brightonchebor <cheborbrighton805@gmail.com>
2025-04-01 13:56:29 +0300

    cn

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,6 +1,8 @@
 import Card from "./components/Card"
 import Counter from "./components/Counter"
 import Navbar from "./components/Navbar"
+
+import Tailwind from "./components/Tailwind";
 import "./App.css";
 
 
@@ -20,10 +22,13 @@ const App = () => {
     // </div>
 
     <>
-      <Navbar />
+      {/* <Navbar />
       <div className="h-screen  bg-[url('/src/assets/mountain.jpg')] bg-cover bg-center before:absolute before:inset-0 before:bg-slate-900 before:opacity-50">
         <div className="z-50"></div>
-      
+      </div> */}
+
+      <div>
+        <Tailwind />
       </div>
     </>
   )
---
+++ movie-app/src/components/Tailwind.jsx
@@ -0,0 +1,11 @@
+
+const Tailwind = () => {
+
+    return(
+        <nav>
+            <h1 class="text-center text-lg text-cyan-400 mt-2">Hello World</h1>
+        </nav>
+    );
+
+}
+export default Tailwind
