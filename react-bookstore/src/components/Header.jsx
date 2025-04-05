4c42bd8245efc0f0104a094da09feae1c8492da9
brightonchebor <cheborbrighton805@gmail.com>
2025-04-05 15:00:40 +0300

    cn

--- react-bookstore/src/components/Header.jsx
+++ react-bookstore/src/components/Header.jsx
@@ -1,6 +1,14 @@
 const Header = () => {
 
-    const headerStyle = {};
+    const headerStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        padding: '10px',
+        position: 'fixed',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+    };
 
     return(
         <div style={headerStyle}>
