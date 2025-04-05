f64066a250c8e53d74955499aa69ee0182330d44
brightonchebor <cheborbrighton805@gmail.com>
2025-04-05 14:48:24 +0300

    cn

--- react-bookstore/src/components/Footer.jsx
+++ react-bookstore/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 
 const Footer = () => {
 
+    const footerStyle = {
+        backgroundColor: 'black',
+        color: 'white',
+        padding: '10px',
+        position: 'fixed',
+        left: '0',
+        bottom: '0',
+        width: '100%',
+        };
     return(
         <div>
-
+            <p style={footerStyle}>
+                This is Footer
+            </p>
         </div>
     );
 }
