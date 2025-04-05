5559247eb49a4a4b23ca3b6a20198ad3a74a05fe
brightonchebor <cheborbrighton805@gmail.com>
2025-04-05 14:37:24 +0300

    cn

--- react-bookstore/src/App.jsx
+++ react-bookstore/src/App.jsx
@@ -1,13 +1,35 @@
-const App = () => {
-  return (
-    <div>
-      <p>
-        We have several books. Feel free to browse for as long as you like.
-        Click on a cover image to see details, or click the Add to Cart button
-        to add a book to your shopping cart.
-      </p>
-    </div>
-  );
-};
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default App;
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
