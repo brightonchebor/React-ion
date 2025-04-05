ef80f9d1bb3346ae2bcf20e2ce695a430d4e37c7
brightonchebor <cheborbrighton805@gmail.com>
2025-04-05 14:36:18 +0300

    cn

---
+++ react-bookstore/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <p>
+        We have several books. Feel free to browse for as long as you like.
+        Click on a cover image to see details, or click the Add to Cart button
+        to add a book to your shopping cart.
+      </p>
+    </div>
+  );
+};
+
+export default App;
