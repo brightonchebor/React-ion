774751003609e45966cf39d61730826932edd423
brightonchebor <cheborbrighton805@gmail.com>
2025-04-06 08:43:33 +0300

    cn

--- react-bookstore/src/App.jsx
+++ react-bookstore/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import Footer from "./components/Footer";
-import Header from "./components/Footer";
+import Header from "./components/Header";
 import "./App.css";
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      
       <Header />
-      <h1>Welcome to React Bookstore</h1>
-      <p>
+      {/* <p>
         We have several books. Feel free to browse for as long as you like.
         Click on a cover image to see details, or click the Add to Cart button
         to add a book to your shopping cart.
@@ -20,7 +21,7 @@ function App() {
           count is {count}
         </button>
       </div>
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
--- react-bookstore/src/components/Header.jsx
+++ react-bookstore/src/components/Header.jsx
@@ -1,20 +1,12 @@
 const Header = () => {
 
-    const headerStyle = {
-        backgroundColor: 'black',
-        color: 'white',
-        padding: '10px',
-        position: 'fixed',
-        left: '0',
-        bottom: '0',
-        width: '100%',
-    };
+   
 
-    return(
-        <div >
-            <p >
-                This is header
-            </p>
-        </div>
-    );
+  return(
+      
+    <h1>Welcome to React Bookstore</h1>
+     
+  );
 }
+
+export default Header
