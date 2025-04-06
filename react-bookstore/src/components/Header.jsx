43d33c559b1b942c7313bb4a6dc3636b60fa18f5
brightonchebor <cheborbrighton805@gmail.com>
2025-04-06 08:51:00 +0300

    cn

--- react-bookstore/src/components/Header.jsx
+++ react-bookstore/src/components/Header.jsx
@@ -1,12 +1,19 @@
 const Header = () => {
+  const headerStyle = {
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    textAlign: "center",
+    fontSize: "1rem",
+    padding: "0.5rem",
+    background: "#f9f9f9",
+  };
 
-   
-
-  return(
-      
-    <h1>Welcome to React Bookstore</h1>
-     
+  return (
+    <header style={headerStyle}>
+      <h2 style={{ margin: 0 }}>Welcome to React Bookstore</h2>
+    </header>
   );
-}
+};
 
-export default Header
+export default Header;
