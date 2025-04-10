ec6699eef9174bafab702cbfa8f45bd84b5e08fd
brightonchebor <cheborbrighton805@gmail.com>
2025-04-10 15:31:40 +0300

    cn

---
+++ biggest-react/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to my Webiste
+      </h1>
+      <nav>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact Us</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
