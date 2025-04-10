30d9188477776d762c5059b48aa53f68d89e5845
brightonchebor <cheborbrighton805@gmail.com>
2025-04-10 16:04:13 +0300

    cn

---
+++ biggest-react/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        Copyright 2025 my website
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ biggest-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <main>
+        <h2>
+            Main Content
+        </h2>
+        <p>
+            This is the main content
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default Main
