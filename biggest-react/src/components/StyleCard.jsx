b1dd51c164eda5d92acc2d61405200db38e5d730
brightonchebor <cheborbrighton805@gmail.com>
2025-04-11 00:37:04 +0300

    cn

--- biggest-react/src/components/StyleCard.jsx
+++ biggest-react/src/components/StyleCard.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const StyleCard = () => {
+function StyleCard() {
   return (
     <div
       style={{
-        backgroundColor: "lightpink",
-        padding: "90px",
-        borderRadius: "90px",
-        color: "darkblue",
+        backgroundColor: 'maroon',
+        color: 'white',
+        padding: "25px",
+        borderRadius: '20px',
+
+        // backgroundColor: "lightblue",
+        // padding: "20px",
+        // borderRadius: "10px",
+        // color: "darkblue",
       }}
     >
-      <h1 style={{ fontSize: "2rem", marginBottom: "2rem" }}>Styled Card</h1>
+      <h2 style={{ fontSize: "3rem", marginBottom: "2rem" }}>Hello World</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        consequatur deserunt accusantium laborum. Totam ab in eos. Eos
-        laboriosam dolorum commodi quia sapiente inventore beatae fugit qui
-        recusandae ipsa ea quae asperiores, corrupti libero! Ut rerum maiores
-        soluta harum nihil magnam asperiores libero placeat, nostrum animi natus
-        laudantium. Molestiae esse neque enim exercitationem hic inventore sint
-        non debitis a quam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero aspernatur esse blanditiis consectetur eius neque iusto provident cum ullam dolores id, magnam doloremque fugiat nisi natus officia magni maxime dolor!
       </p>
     </div>
-  )
+  );
 }
 
-export default StyleCard
+export default StyleCard;
