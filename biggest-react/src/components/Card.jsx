b46ce0a9c9c16013e8ccc0f6738bef9975911325
brightonchebor <cheborbrighton805@gmail.com>
2025-04-11 11:56:38 +0300

    cn

---
+++ biggest-react/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Card() {
+
+    const cardStyle = {
+
+    };
+
+    const h1Style = {
+
+    };
+
+    const pStyle = {
+
+    };
+
+  return (
+    
+    <div>
+      <h1>My Card</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa in debitis nobis repellendus veritatis tempora voluptas perspiciatis cum natus, aliquam hic asperiores molestias, saepe, enim inventore eligendi. Quia, repellat dicta.</p>
+    </div>
+  )
+}
+
+export default Card
