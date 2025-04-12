6b9eded950924a1c3a9f3e90938e4acc9503088b
brightonchebor <cheborbrighton805@gmail.com>
2025-04-12 22:46:43 +0300

    cn

--- biggest-react/src/components/EventDemo.jsx
+++ biggest-react/src/components/EventDemo.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const EventDemo = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [button, setButton] = useState('Button Clicked')
+
+    return (
+        <div>
+
+            <div>
+            <button></button>
+            <p></p>
+            </div>
+            
+        </div>
+    )
 }
 
 export default EventDemo
