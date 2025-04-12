60a204ba7ac7166c51e244c525fa99588a32e2c8
brightonchebor <cheborbrighton805@gmail.com>
2025-04-12 23:08:40 +0300

    cn

--- biggest-react/src/components/EventDemo.jsx
+++ biggest-react/src/components/EventDemo.jsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import { GiButtonFinger } from 'react-icons/gi'
-
+import React from "react";
+import { useState } from "react";
+import { GiButtonFinger } from "react-icons/gi";
 
 const EventDemo = () => {
+  const [button, setButton] = useState("Click Me!");
+  const handleButton = () => {
+    setButton("Button Clicked");
+  };
 
-    const [button, setButton] = useState('Click Me!')
-    const handleButton = () => {
-        setButton('Button Clicked')
-    }
+  const [copy, setCopy] = useState("Copy Me!");
+  const handleCopy = () => {
+    setCopy("Text Copied");
+  };
 
-    const [copy, setCopy] = useState('Copy Me!')
-    const handleCopy = () => {
-        setCopy('Text Copied')
-    }
+  const [hover, setHover] = useState({});
+  const handleHover = () => {
+    setHover({ backgroundColor: "lightyellow" });
+  };
+  const handleHoverOut = () => {
+    setHover({});
+  };
 
-    const [hover, setHover] = useState('Hover over me!')
-    const handleHover = () => {
-        setHover({backgroundColor: 'lightyellow'})
-    }
-    const handleHoverOut = () => {
-        setHover({})
-    }
+  return (
+    <div>
+      <div>
+        <button onClick={handleButton}>{button}</button>
+        <p></p>
+      </div>
 
-    
-    return (
-        <div>
+      <div>
+        <p onCopy={handleCopy}>{copy}</p>
+      </div>
 
-            <div>
-            <button onClick={handleButton}>{button}</button>
-            <p></p>
-            </div>
+      <div>
+        <p
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          style={hoverStyle}
+        >
+          Hover over me!
+        </p>
+      </div>
+    </div>
+  );
+};
 
-            <div>
-                <p onCopy={handleCopy}>{copy}</p>
-            </div>
-
-        </div>
-    )
-}
-
-export default EventDemo
+export default EventDemo;
