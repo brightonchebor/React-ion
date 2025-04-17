2d5822b799eb0694e683f23ddd305c30d064be61
brightonchebor <cheborbrighton805@gmail.com>
2025-04-17 10:36:45 +0300

    cn

--- biggest-react/src/App.jsx
+++ biggest-react/src/App.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-// import User from './components/User';
-// import Password from './components/Password';
-import StyleCard from './components/StyleCard';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CardList from './components/CardList';
-import ProfileCard from './components/ProfileCard';
-import IconCOmponent from './components/IconCOmponent';
-import EventDemo from './components/EventDemo';
 import './App.css';
+import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div>
-      {/* <Header /> */}
-    
-      {/* <StyleCard />
-      <StyleCard />
-      <StyleCard /> */}
-
-      {/* <ProfileCard /> */}
-      {/* <Footer /> */}
-      <h1>React event handling challenge</h1>
-      <EventDemo />
+      <Counter />
     </div>
   )
 }
--- biggest-react/src/components/Counter.jsx
+++ biggest-react/src/components/Counter.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 const Counter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    
+    const [count, setCount] = useState(0)
+    const myCount = () => setCount(count++)
+    return (
+        <div>
+        <p onClick={myCount}>Count: {count}</p>
+        </div>
+    )
 }
 
 export default Counter
