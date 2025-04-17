21da6844db5f8d8592ca4c15e216d8a9aacacfbb
brightonchebor <cheborbrighton805@gmail.com>
2025-04-17 10:20:25 +0300

    cn

--- biggest-react/src/components/EventDemo.jsx
+++ biggest-react/src/components/EventDemo.jsx
@@ -3,21 +3,21 @@ import { useState } from "react";
 import { GiButtonFinger } from "react-icons/gi";
 
 const EventDemo = () => {
-  // const [button, setButton] = useState("Click Me!");
-  // const handleButton = () => {
-  //   setButton("Button Clicked");
-  // };
+  const [button, setButton] = useState("Click Me!");
+  const handleButton = () => {
+    setButton("Button Clicked");
+  };
 
-  const Button = () => {
-    const handleClick = () => {
-      console.log(Math.round(Math.random() * 10));
-    }
-    return (
-      <button onClick={handleClick}>
-        Click Me
-      </button>
-    )
-  }
+  // const Button = () => {
+  //   const handleClick = () => {
+  //     console.log(Math.round(Math.random() * 10));
+  //   }
+  //   return (
+  //     <button onClick={handleClick}>
+  //       Click Me
+  //     </button>
+  //   )
+  // }
 
   const [copy, setCopy] = useState("Copy Me!");
   const handleCopy = () => {
@@ -32,13 +32,30 @@ const EventDemo = () => {
     setHover({});
   };
 
+
+  const Move = () => {
+    function moveHandler() {
+      alert("Mouse Move Event Fired");
+      console.log("Mouse Move Event Fired");
+    }
+  
+    return (
+      <p onMouseOver={moveHandler} style={{ backgroundColor: "lightyellow" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        voluptatibus libero, eius odit modi doloremque quos magni tempora. Debitis
+        laborum exercitationem perferendis. Veritatis fuga, quod quasi accusamus
+        eveniet voluptates suscipit.
+      </p>
+    );
+  };
+
   return (
     <div>
       <div>
-        {/* <button onClick={handleButton}>{button}</button>
-        <p></p> */}
+        <button onClick={handleButton}>{button}</button>
+        <p></p>
 
-        <Button />
+        {/* <Button /> */}
       </div>
 
       <div>
@@ -53,6 +70,10 @@ const EventDemo = () => {
         >
           Hover over me!
         </p>
+      </div>
+
+      <div>
+        <Move />
       </div>
     </div>
   );
