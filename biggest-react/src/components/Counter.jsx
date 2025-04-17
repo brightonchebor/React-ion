3a8ad5e00c7a180db1bbee8441a5dab72d691814
brightonchebor <cheborbrighton805@gmail.com>
2025-04-17 10:54:48 +0300

    Cn

--- biggest-react/src/components/Counter.jsx
+++ biggest-react/src/components/Counter.jsx
@@ -7,12 +7,19 @@ const Counter = () => {
     const myCount = () => setCount(
         prevCount => prevCount + 1
     );
+    const buttonStyle = {
+        padding: '10px',
+        margin: '20px',
+        fontSize: '2rem',
+        color: 'black',
+    }
     return (
-      <div className="p-4 text-center">
-        <h1 className="text-2xl font-bold mb-4">Counter: {count}</h1>
+      <div >
+        <h1 >Counter: {count}</h1>
         <button
           onClick={myCount}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          style={buttonStyle}
+          
         >
           Increment
         </button>
