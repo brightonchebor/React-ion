988d6af208a69895623527a26902df6d37f38ee5
brightonchebor <cheborbrighton805@gmail.com>
2025-04-19 11:08:54 +0300

    Cn

--- tubidyui/src/components/SearchBar.jsx
+++ tubidyui/src/components/SearchBar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 const SearchBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const searchBar = ({onSearch}) => {
+        const [query, setQuery] = useState('');
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            onSearch(query)
+        }
+    }
+    return (
+        <form className='search-bar' onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            placeholder='Enter Keyword or Youtube URL here...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)} 
+            />
+            <button type='submit' className='search-button'>
+                <i className='search-icon'>🔍</i>
+                Search
+            </button>
+        </form>
+    )
 }
 
 export default SearchBar
