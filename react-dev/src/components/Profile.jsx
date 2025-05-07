69b7395ad085d3712035aac8fdd7c638d010fffd
brightonchebor <cheborbrighton805@gmail.com>
2025-05-07 11:29:50 +0300

    cn

---
+++ react-dev/src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Profile
+
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
+  
