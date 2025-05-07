da52597e5c1abe896c279e147e1a0f13566db431
brightonchebor <cheborbrighton805@gmail.com>
2025-05-07 11:48:25 +0300

    cn

--- react-dev/src/components/Profile.jsx
+++ react-dev/src/components/Profile.jsx
@@ -3,30 +3,15 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
-      
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
     </div>
   )
 }
 
 export default Profile
 
-function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/MK3eW3As.jpg"
-        alt="Katherine Johnson"
-      />
-    );
-  }
-  
-  export default function Gallery() {
-    return (
-      <section>
-        <h1>Amazing scientists</h1>
-        <Profile />
-        <Profile />
-        <Profile />
-      </section>
-    );
-  }
+
   
