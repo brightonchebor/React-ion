cdfc1ff931fcc86a839b40636c10d3abf4ca407d
brightonchebor <cheborbrighton805@gmail.com>
2025-07-27 08:17:38 +0300

    cn

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className='bg-red-500 p-20'>
+      <h1 className='font-bold text-white text-5xl'>
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
